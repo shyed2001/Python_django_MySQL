9176a6618f1ad790030b7d564908871f6fcf7eb4
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-12-10 15:17:18 +0600

    ok
    ok

--- SmallDjangoProject/SmallDjangoWebAPP/firstApp/urls.py
+++ SmallDjangoProject/SmallDjangoWebAPP/firstApp/urls.py
@@ -3,7 +3,28 @@ from django.urls import path
 from firstApp import views
 urlpatterns = [
     path('', views.index, name='Home' ),
+    path('index', views.index, name='Home' ),
     path('about', views.about, name='about'),
-    path('services', views.services, name='services'),
-    path('contact', views.contact, name='contact')
+    path('pages', views.pages, name='pages'),
+    path('documentations', views.documentations, name='documentations'),
+    path('services', views.service, name='services'),
+    path('blogs', views.blogs, name='blogs'),
+    path('directions', views.directions, name='directions'),
+    path('products', views.products, name='products'),
+    path('locations', views.locations, name='locations'),
+    path('links', views.links, name='links'),
+    path('contacts', views.contacts, name='contacts'),   
+   
+   ## For compatibility with general html code 
+    path('index.html', views.index, name='Home' ),
+    path('about.html', views.about, name='about'),
+    path('pages.html', views.pages, name='pages'),
+    path('documentations.html', views.documentations, name='documentations'),
+    path('services.html', views.service, name='services'),
+    path('blogs.html', views.blogs, name='blogs'),
+    path('directions.html', views.directions, name='directions'),
+    path('products.html', views.products, name='products'),
+    path('locations.html', views.locations, name='locations'),
+    path('links.html', views.links, name='links'),
+    path('contacts.html', views.contacts, name='contacts')
 ]
--- SmallDjangoProject/SmallDjangoWebAPP/firstApp/views.py
+++ SmallDjangoProject/SmallDjangoWebAPP/firstApp/views.py
@@ -14,15 +14,44 @@ def about(request):
   #return HttpResponse("This is About Page")
   return render(request, 'about.html', context)
 
+def pages(request):
+  #return HttpResponse("This is About Page")
+  return render(request, 'pages.html', context)
 
-def services(request):
+def documentations(request):
+  #return HttpResponse("This is About Page")
+  return render(request, 'documentations.html', context)
+
+def service(request):
   return render(request, 'services.html', context)
-  
-  
+
   """ return HttpResponse('''This is About services page \n
                       This is About services page \n
                       This is About services page ''') """
+                        
+def blogs(request):
+  return render(request, 'blogs.html', context)
 
-def contact(request):
-  return render(request, 'contact.html', context)
+def directions(request):
+  return render(request, 'directions.html', context)
+
+
+def products(request):
+  return render(request, 'products.html', context)
+
+
+def locations(request):
+  return render(request, 'locations.html', context) 
+
+def links(request):
+  return render(request, 'links.html', context) 
+
+
+def contacts(request):
+  context={
+    "WebmasterName":"Name- Shyed Shahriar Housaini",
+    "WebmasterEmail":"E-mail- shyed2001@yahoo.com",
+    "Website":"Website- https://www.digialbd.org"
+  }
+  return render(request, 'contacts.html', context)
   # return HttpResponse("This is About contact page")
