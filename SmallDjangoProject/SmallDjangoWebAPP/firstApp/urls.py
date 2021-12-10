0c589e2edf5bdd768f4f8810effd5992a98775f3
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-12-10 13:32:57 +0600

    ok
    ok

--- SmallDjangoProject/SmallDjangoWebAPP/firstApp/urls.py
+++ SmallDjangoProject/SmallDjangoWebAPP/firstApp/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from firstApp import views
 urlpatterns = [
-    path('', views.index, name='home' ),
+    path('', views.index, name='Home' ),
     path('about', views.about, name='about'),
     path('services', views.services, name='services'),
     path('contact', views.contact, name='contact')
--- SmallDjangoProject/SmallDjangoWebAPP/firstApp/views.py
+++ SmallDjangoProject/SmallDjangoWebAPP/firstApp/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
   context={
-    "variable1":"Shyed",
-    "variable2":"Shahriar",
-    "variable3":"Housaini",
+    "WebmasterName":"Name- Shyed Shahriar Housaini",
+    "WebmasterEmail":"E-mail- shyed2001@yahoo.com",
+    "Website":"Website- https://www.digialbd.org"
   }
   return render(request, 'index.html', context)
  # return HttpResponse("This is Home Page")
 
 def about(request):
   #return HttpResponse("This is About Page")
-  return render(request, 'about.html')
+  return render(request, 'about.html', context)
 
 
 def services(request):
-  return render(request, 'services.html')
+  return render(request, 'services.html', context)
   
   
   """ return HttpResponse('''This is About services page \n
@@ -24,5 +24,5 @@ def services(request):
                       This is About services page ''') """
 
 def contact(request):
-  return render(request, 'contact.html')
+  return render(request, 'contact.html', context)
   # return HttpResponse("This is About contact page")
